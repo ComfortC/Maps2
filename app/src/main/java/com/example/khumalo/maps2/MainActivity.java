bbf61377bba0796e57ad9a65d3fce56f2403f1ef
ComfortChinondiwana <comfortchino@gmail.com>
2016-07-11 14:06:35 +0200

    Location co-ordinates

--- app/src/main/java/com/example/khumalo/maps2/MainActivity.java
+++ app/src/main/java/com/example/khumalo/maps2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.khumalo.maps2;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +10,93 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    protected static final String Tag = "Locations Services Lesson";
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected TextView mLatitudetext;
+    protected TextView mLongtudetext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLatitudetext = (TextView)findViewById(R.id.latitude_text_display);
+        mLongtudetext= (TextView)findViewById(R.id.longitude_text_display);
+
+        buildGoodleApiClient();
+
+    }
+
+    private void buildGoodleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+           mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation!=null){
+            mLatitudetext.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongtudetext.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 
